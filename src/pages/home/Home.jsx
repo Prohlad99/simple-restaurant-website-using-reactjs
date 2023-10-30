4a5ca397a4f386c24309b4bff13b0fe20b5ed938
Prohlad99 <prohlad.m99@gmail.com>
2023-10-30 17:05:56 +0600

    animation added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 
 import { Helmet } from 'react-helmet-async';
+import { Fade, Flip, Zoom } from 'react-reveal';
 import BistroBanner from '../../components/home/banner/bistro/BistroBanner';
 import MenuBanner from '../../components/home/banner/menu/MenuBanner';
 import Menu from '../../components/home/menu/Menu';
@@ -16,19 +17,33 @@ const Home = () => {
       </Helmet>
       <HeroCarousel />
       <MenuHeader title="From 11:00am to 10:00pm" subtitle="ORDER ONLINE" />
-      <ItemSlider />
-      <BistroBanner />
+      <Zoom>
+        <ItemSlider />
+      </Zoom>
+      <Flip left>
+        <BistroBanner />
+      </Flip>
       <MenuHeader title="Check it out" subtitle="FROM OUR MENU" />
-      <Menu />
+      <Zoom>
+        <Menu />
+      </Zoom>
       <MenuHeader title="Should Try" subtitle="CHEF RECOMMENDS" />
-      <Recommends />
-      <div className="md:hidden">
-        <MenuHeader title="Check it out" subtitle="FROM OUR MENU" />
-      </div>
-      <MenuBanner />
+      <Fade left>
+        <Recommends />
+      </Fade>
+      <Zoom>
+        <div className="md:hidden">
+          <MenuHeader title="Check it out" subtitle="FROM OUR MENU" />
+        </div>
+      </Zoom>
+      <Zoom>
+        <MenuBanner />
+      </Zoom>
       <MenuHeader title="What Our Clients Say" subtitle="TESTIMONIALS" />
       {/* <Testimonial /> */}
-      <Testimonials/>
+      <Zoom>
+        <Testimonials />
+      </Zoom>
     </div>
   );
 }
